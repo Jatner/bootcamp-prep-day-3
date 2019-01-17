b235a5ba8d2f551019be3fdca903a61e35d654cf
Jatner <jatner@gmail.com>
2019-01-16 20:42:30 -0800

    Bunch who knows what

--- main.js
+++ main.js
@@ -1,2 +1,44 @@
-var sum = 3 + 4;
-console.log("hello console!", sum)
+var x = 14;
+
+if(x > 12) {
+
+    console.log(x);
+    console.log('grooving with if');
+}
+
+
+else {
+    console.log('too small');
+
+}
+// 1. initialize
+// 2. Condition
+// 3. Post Loop Action 
+for(var i = 0; i < x; i++) {
+    console.log(i);
+
+}
+
+function sayHello() {
+
+    console.log('hi');
+}
+if(x % 2 === 0) {
+    sayHello();
+}
+
+function evenOrOdd(){
+
+    var y = 4; 
+
+    if( y % 2 === 0) {
+
+        console.log('even');
+    }
+
+    else {
+        console.log('odd');
+    }
+
+
+}
